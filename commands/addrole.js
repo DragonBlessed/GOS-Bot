b81c3145eaaa09df432529f4406e9f422aaefe3d
DragonBlessed <christianvasil72@gmail.com>
2023-10-08 15:42:44 -0700

    added embeds to add role command

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, PermissionsBitField, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -13,7 +13,17 @@ module.exports = {
       option
         .setName('roles')
         .setDescription('The roles to add, separated by commas. Max: 5')
-        .setRequired(true)),
+        .setRequired(true))
+    .addStringOption(option =>
+      option
+        .setName('title')
+        .setDescription('The title of the embed.')
+        .setRequired(false))
+    .addStringOption(option =>
+      option
+        .setName('media')
+        .setDescription('Add a gif/image url to the message.')
+        .setRequired(false)),
   async execute(interaction) {
     try {
       const userId = interaction.user.id;
@@ -27,6 +37,26 @@ module.exports = {
 
       if (!isDeveloper && !isAdmin) {
         await interaction.reply({ content: 'You do not have the required permissions to use this command.', ephemeral: true });
+        return;
+      }
+
+      const media = interaction.options.getString('media');
+      const title = interaction.options.getString('title');
+
+      // Media URL Validation
+      const mediaExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+      if (media) {
+        const url = new URL(media);  
+        const extension = url.pathname.split('.').pop();
+        if (!mediaExtensions.includes(extension.toLowerCase())) {
+          await interaction.reply({ content: 'Invalid media extension. Only jpg, jpeg, png, and gif are supported.', ephemeral: true });
+          return;
+        }
+      }
+  
+      // Title Length Check
+      if (title && title.length > 256) {
+        await interaction.reply({ content: 'Title too long, must be 256 characters or fewer.', ephemeral: true });
         return;
       }
 
@@ -45,12 +75,53 @@ module.exports = {
         row.addComponents(button);
       }
       
-      await interaction.reply({
-        content: messageContent,
-        components: [row]
-      });
+      if (media && title) {
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(messageContent)
+              .setImage(media)
+              .setTitle(title)
+          ],
+          components: [row]
+        });
+      }
+      else if (media) {
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(messageContent)
+              .setThumbnail(media)
+          ],
+          components: [row]
+        });
+      }
+      else if (title) {
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(messageContent)
+              .setTitle(title)
+          ],
+          components: [row]
+        });
+      }
+      else {
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(messageContent)
+          ],
+          components: [row]
+        });
+      } 
 
     } catch (error) {
+      // Handle general errors
+      if (error instanceof TypeError && error.message.includes('Invalid URL')) {
+        await interaction.reply({ content: 'Invalid media URL.', ephemeral: true });
+        return;
+      }
       console.error('Error in addrolesystem:', error);
       try {
         await interaction.reply({ content: 'An error occurred while executing this command.', ephemeral: true });
@@ -58,8 +129,7 @@ module.exports = {
         console.error('Error while sending error message:', replyError);
       }
     }
-  }
+  },  
 };
 
 
-// Add embeds to the addrolesystem command
